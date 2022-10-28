fe5c6cbf702bdab5440f38cc6304590e5278036b
Linalii <36931084+Linalii@users.noreply.github.com>
2022-10-28 22:49:36 +0300

    Add files via upload

---
+++ Task001.py
@@ -0,0 +1,5 @@
+number = int(input())
+if (number == 6) or (number == 7) :
+    print("да")
+else:
+    print("нет")
---
+++ Task003.py
@@ -0,0 +1,9 @@
+number = int(input())
+if number == 1:
+    print("+∞,+∞")
+elif number == 2:
+    print("-∞,+∞")
+elif number == 3:
+    print("-∞,-∞")
+else:
+    print("+∞,-∞")
